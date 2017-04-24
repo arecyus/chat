383f7b7d24a6d1d8402b932bcce9a36c34397b68
Dream --- <Dream --->
2017-04-24 08:29:43 -0300

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 const publicPath = path.join(__dirname, "../public");
 
 const app = express();
 const port= process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=> {
+io.on("connection", (socket)=>{
+    console.log("Nuevo usuario");
+
+    socket.on("disconnect",()=>{
+        console.log("Desconectado del servidor.");
+    })
+});
+
+
+server.listen(port,()=> {
     console.log("Arrancando en port "+port);
 });
