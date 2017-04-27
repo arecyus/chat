da0fe212dff82bfb89fe663eb8831caa24ec6b53
Dream --- <Dream --->
2017-04-27 08:56:25 -0300

    Scroll to bottom if user is close to buttom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
 var socket = io();
 
+function scrollToBottom () {
+  var messages = $("#messages");
+  var newMessage = messages.children("li:last-child");
+  var clientHeight = messages.prop("clientHeight");
+  var scrollTop = messages.prop("scrollTop");
+  var scrollHeight = messages.prop("scrollHeight");
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if(clientHeight+scrollTop + newMessageHeight + lastMessageHeight>=scrollHeight){
+      messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on("connect", function() {
     console.log("Conectado al servidor");
 });
@@ -19,7 +32,7 @@ socket.on("newMessage",function(message){
 
     $("#messages").append(html);
 
-
+    scrollToBottom();
     // console.log("Nuevo mensaje",message);
     // var li = $("<li></li>");
     // li.text(message.from+" "+formattedTime+" :"+ message.text);
@@ -38,6 +51,7 @@ socket.on("newLocationMessage", function (message) {
     });
 
     $("#messages").append(html);
+    scrollToBottom();
     // var li = $("<li></li>");
     // var a = $('<a target="_blank">Mi posicion</a>');
     //
